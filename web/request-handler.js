cfca6069426fc225dbd21a2a54795445431b212f
Hack Reactor Students <jygsaw@gmail.com>
2014-06-26 12:36:47 -0700

    Implement GET request

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,39 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var http = require('./http-helpers');
+var url = require('url');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  console.log("HELLO: " + req.method);
+  console.log("URL: ", req.url);
+
+  var statusCode = null;
+  var data = null;
+
+  var handlerFuncs = {};
+  handlerFuncs.GET = function() {
+    console.log("=== GET request ===");
+    statusCode = 200;
+    if (req.url === '/') {
+      data = archive.paths.siteAssets + '/index.html';
+    } else if (req.url.match(/www/)) {
+      data = archive.paths.archivedSites + req.url;
+    } else {
+      data = archive.paths.siteAssets + req.url;
+    }
+  };
+  handlerFuncs.POST = function() {
+    console.log("=== POST request ===");
+    statusCode = 302;
+  };
+
+  if (handlerFuncs[req.method]) {
+    handlerFuncs[req.method]();
+  } else {
+    statusCode = 500;
+  }
+
+  // res.writeHead(statusCode, http.headers);
+  http.serveAssets(res, data, statusCode);
 };
